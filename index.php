316ae72df5468003d9019a44a726e5392e7578be
Michael Ajetunmobi <mikoloxtra@gmail.com>
2018-04-14 06:23:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="overlay">
+		<h1 class="weltext">WELCOME TO HNG INTERNSHIP</h1>
+		<h1 class="time">THE TIME IS :<?php date_default_timezone_get('Nigeria/Lagos');
+			echo date("h:i:sa"); ?>	
+		</h1>
+		<b><p class="footer">Brought to you by HNG Internship  |  &copy copyright 2018</p></b>
+	</div>
+</body>
+</html>
